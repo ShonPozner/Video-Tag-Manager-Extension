cf7748934412d91f410b96c4dc59244f7cbcddc7
SHON POZNER <shon4me@gmail.com>
2021-03-25 12:30:35 +0200

    commit befor try add popup

--- public/background.js
+++ public/background.js
@@ -1,6 +1,11 @@
 /* global chrome */
 
 chrome.browserAction.onClicked.addListener(function(tab) {
+
+  //Show Popup window: 1. View the summary 2. Displays the Add New Tag butto
+
+  
+
   console.log("1. backGround: tab - ",  tab)
   console.log("2. backGround: tab.id - "+  tab.id)
   chrome.tabs.sendMessage(tab.id, { message: 'load' });
